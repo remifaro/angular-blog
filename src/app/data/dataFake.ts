43c999651d506f5b54061c11c0729817574327df
remifaro <faroremi1@gmail.com>
2025-03-11 22:06:17 -0300

    Página de conteúdo criada

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"O Santa Cruz agora é SAF",
+        "description": "Clique e saiba mais sobre a SAF do Santa Cruz, o Terror do Nordeste", 
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnSsVEhNVLRWnUXds8JJwrtgf9fG3w4gAcWQ&s"
+    },
+    {
+        "id":"2",
+        "title":"Santa assina Vinculante da SAF",
+        "description": "lorem ipsum lorem ipsum lorem ipsum",
+        "photoCover":"https://imagens.ne10.uol.com.br/veiculos/_midias/jpg/2025/01/13/597x330/1_saf_santa_cruz-33520573.jpeg"
+    }
+]
