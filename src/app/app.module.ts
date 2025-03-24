3ef414821fbcb96a53c0668e0e941dcdcc27c0dc
remifaro <faroremi1@gmail.com>
2025-03-24 18:53:49 -0300

    Adição de menu-sanduiche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
+import { MenuSanduicheComponent } from './components/menu-sanduiche/menu-sanduiche.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ContentComponent } from './pages/content/content.component';
     BigCardComponent,
     SmallCardComponent,
     HomeComponent,
-    ContentComponent
+    ContentComponent,
+    MenuSanduicheComponent
   ],
   imports: [
     BrowserModule,
